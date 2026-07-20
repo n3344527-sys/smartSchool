949827b6780a826acb40551135b8215abe6c7113
n3344527-sys <n3344527@gmail.com>
2026-07-20 23:40:15 +0530

    Update api.js

--- Frontend/frontend/src/services/api.js
+++ Frontend/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const backendApi = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://smartschool-backend-cx5b.onrender.com",
   withCredentials: true,
 });
 
